6875bc94ddfe40cab95f283002af80d3db7300c4
JMJ <ghtntkdnsk@naver.com>
2025-11-10 00:54:52 +0900

    feat(patterns.js): 패턴 관련 상수들을 관리하는 모듈 추가

---
+++ src/constants/patterns.js
@@ -0,0 +1,4 @@
+export const CUSTOM_DELIMITER_PATTERN = /^\/\/(.)(\\n|\n)/;
+export const REGEX_ESCAPE_PATTERN = /[-\/\\^$*+?.()|[\]{}]/g;
+export const NUMBER_FILTER_PATTERN = /[^0-9.\-\s]/g;
+export const WHITESPACE_PATTERN = /\s+/g;
