364cd5180b90e4d3a7bd6157f30986a121d3ebd4
JMJ <ghtntkdnsk@naver.com>
2025-11-10 15:47:07 +0900

    refactor(StringCalculator): 숫자 검증에 관한 로직을 Parser로 옮김

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,7 +3,6 @@ import { validateNumbers } from "./Validator.js";
 
 export function add(input) {
   const numbers = parseToNumbers(input);
-  validateNumbers(numbers);
 
   return numbers.reduce((total, number) => total + number, 0);
 }
